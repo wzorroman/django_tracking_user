2f9d617a4f1a9822000e5216b7d4f46481ea671a
wilson zauma <wzaumarojas@gmail.com>
2020-09-05 13:51:33 -0500

    agregando modelo status auditable

--- tracking_user/admin.py
+++ tracking_user/admin.py
@@ -21,3 +21,52 @@ class AuditableAdmin(admin.ModelAdmin):
                     instance.created_by = request.user
                 instance.modified_by = request.user
                 instance.save()
+
+class Auditable2Admin(admin.ModelAdmin):
+    exclude = ('created_by', 'modified_by',)
+    readonly_fields = ('created_by', 'modified_by', 'created', 'modified')
+    
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in instances:
+            if isinstance(instance, Auditable2Model):  # Check if it is the correct type of inline
+                if not instance.created_by_id:
+                    instance.created_by = "{} {}".format(request.user.id, request.user.username)
+                instance.modified_by = "{} {}".format(request.user.id, request.user.username)
+                instance.save()
+
+    def format_created(self, obj):
+        return obj.created.strftime("%d/%m/%Y %H:%M:%S") if obj.created else '-'
+
+    def format_modified(self, obj):
+        return obj.modified.strftime("%d/%m/%Y %H:%M:%S") if obj.modified else '-'
+
+    format_created.short_description = 'Fecha creacion'
+    format_created.admin_order_field = 'created'
+    format_modified.short_description = 'Fecha edicion'
+    format_modified.admin_order_field = 'modified'
+
+
+class StatusAuditable2Admin(admin.ModelAdmin):
+    exclude = ('termination_by',)
+    readonly_fields = ('termination', 'termination_by')
+    list_display = ('format_termination', 'termination_by', 'estado')
+    list_filter = ('estado', )
+
+    def format_termination(self, obj):
+        return obj.termination.strftime("%d/%m/%Y %H:%M:%S") if obj.termination else '-'
+
+    format_termination.short_description = 'Fecha terminacion'
+    format_termination.admin_order_field = 'termination'
+
+    # funcion q debe incluirse en la herencia del admin para list_display
+    # def get_list_display(self, request):
+    #     # campos del modelo a mostrar en el listado
+    #     lista_mostrar = ('id', 'nombre')
+    #     return lista_mostrar + self.list_display
+
+    # funcion q debe incluirse en la herencia del admin para list_filter
+    # def get_list_filter(self, request):
+    #     # campos del modelo a filtrar
+    #     filtros = ('tipo',)
+    #     return self.list_filter + filtros
--- tracking_user/models.py
+++ tracking_user/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import datetime
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
@@ -8,16 +9,19 @@ from django.conf import settings
 # audit
 from crum import get_current_user
 
+from apps.common import constants
+
 
 class AuditableModel(models.Model):
-    created = models.DateTimeField(auto_now_add = True)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_created_by')
-
-    modified = models.DateTimeField(auto_now = True)
-    modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_modidied_by')
+    created = models.DateTimeField(auto_now_add=True, db_column='FEC_CREACION')
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_created_by',
+                                   db_column='USR_CREACION', on_delete=models.PROTECT)
+    modified = models.DateTimeField(auto_now=True, db_column='FEC_EDICION')
+    modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_modidied_by',
+                                    db_column='USR_EDICION', on_delete=models.PROTECT)
 
     class Meta:
-        abstract = True
+        abstract = True        
     
     def save(self, *args, **kwargs):
         user = get_current_user()
@@ -27,3 +31,65 @@ class AuditableModel(models.Model):
             self.created_by = user
         self.modified_by = user
         super(AuditableModel, self).save(*args, **kwargs)
+
+
+class StatusAuditableModel(models.Model):
+    termination = models.DateTimeField("Fecha baja", db_column='FECHA_BAJA', null=True, blank=True)
+    termination_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='%(class)s_termination',
+                                       db_column='USR_BAJA', on_delete=models.PROTECT, null=True, blank=True)
+    estado = models.CharField("Estado", db_column='ESTADO', max_length=1, choices=constants.ESTADO_GENERICO_CHOICES,
+                              default=constants.ESTADO_GENERICO_HABILITADO)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        user = get_current_user()
+        if self.estado == constants.ESTADO_GENERICO_DESHABILITADO:
+            self.termination = datetime.now()
+            self.termination_by = user
+        else:
+            self.termination = None
+            self.termination_by = None
+        super(StatusAuditableModel, self).save(*args, **kwargs)
+
+
+# -- auditoria sin relaciones al user
+
+class Auditable2Model(models.Model):
+    created = models.DateTimeField(auto_now_add=True, db_column='FEC_CREACION')
+    created_by = models.CharField("Created by", max_length=50, db_column='USR_CREACION')
+    modified = models.DateTimeField(auto_now=True, db_column='FEC_EDICION')
+    modified_by = models.CharField("Modified by", max_length=50, db_column='USR_EDICION')
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        user = get_current_user()
+        if user and not user.pk:
+            user = None
+        if not self.pk:
+            self.created_by = "{} {}".format(user.id, user.username)
+        self.modified_by = "{} {}".format(user.id, user.username)
+        super(Auditable2Model, self).save(*args, **kwargs)
+
+
+class StatusAuditable2Model(models.Model):
+    termination = models.DateTimeField("Fecha baja", db_column='FECHA_BAJA', null=True, blank=True)
+    termination_by = models.CharField("Baja por", max_length=50, db_column='USR_BAJA', null=True, blank=True)
+    estado = models.CharField("Estado", db_column='ESTADO', max_length=1, choices=constants.ESTADO_GENERICO_CHOICES,
+                              default=constants.ESTADO_GENERICO_HABILITADO)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        user = get_current_user()
+        if self.estado == constants.ESTADO_GENERICO_DESHABILITADO:
+            self.termination = datetime.now()
+            self.termination_by = "{} {}".format(user.id, user.username)
+        else:
+            self.termination = None
+            self.termination_by = None
+        super(StatusAuditable2Model, self).save(*args, **kwargs)
