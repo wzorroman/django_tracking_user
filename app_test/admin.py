559a6af334b50742b6fb57af4bc77fb99cb8915e
wilson zauma <wzaumarojas@gmail.com>
2020-09-05 13:58:31 -0500

    implementando modelo de ejemplo con statusAuditable

--- app_test/admin.py
+++ app_test/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-#from tracking_user.admin import AuditableAdmin
+from tracking_user.admin import AuditableAdmin
 
 # Register your models here.
 from app_test.models import *
@@ -12,3 +12,20 @@ class Tabla01Admin(admin.ModelAdmin):
     readonly_fields = ('created_by','modified_by','created', 'modified')
 
 admin.site.register(Tabla01, Tabla01Admin)
+
+
+class Tabla02Admin(StatusAuditable2Admin):
+    # list_display = ('id', 'descripcion', 'format_termination', 'termination_by', 'estado')
+    # list_filter = ('estado', 'tipo')
+    search_fields = ('id', 'descripcion')
+
+    def get_list_display(self, request):
+        lista_mostrar = ('id', 'descripcion', 'tipo')
+        return lista_mostrar + self.list_display
+
+    def get_list_filter(self, request):
+        filtros = ('tipo',)
+        return self.list_filter + filtros
+
+
+admin.site.register(Tabla02, Tabla02Admin)
--- app_test/models.py
+++ app_test/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from tracking_user.models import AuditableModel
+from tracking_user.models import AuditableModel, StatusAuditable2Model
 # Create your models here.
 
 class Tabla01(AuditableModel):
@@ -12,3 +12,18 @@ class Tabla01(AuditableModel):
 
     def __unicode__(self):
         return self.nombre
+
+class Tabla02(StatusAuditable2Model):
+    TIPO_OPC_MERCADO = 'MERCADO'
+    TIPO_OPC_VIVIENDA = 'VIVIENDA'
+
+    TIPO_CHOICES = (
+        (TIPO_OPC_MERCADO, 'Mercado'),
+        (TIPO_OPC_VIVIENDA, 'Vivienda'),
+    )
+    descripcion = models.CharField('Nombre', max_length=100)
+    comentario = models.TextField('Comentario', null=True, blank=True)
+    tipo = models.CharField("Tipo",max_length=50, choices=TIPO_CHOICES)
+
+    def __unicode__(self):
+        return self.descripcion
